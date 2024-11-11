176ebba08df6d741ae428031e9e82cbec2121be4
Gabriel Neves <ga.sadauscas@gmail.com>
2024-11-11 18:01:02 -0300

    Feat: Arrumei o Json

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,20 @@ const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('foggers do poggers')
+  res.send('poggers')
+})
+
+app.get('/dados', (req, res) => {
+ res.send('foggers')
+})
+
+app.post('/json', (req, res) => {
+const dados  = req.body
+console.log (dados.nome)
+res.json({ message: 'lumbertycoon', dados })
 })
 
 app.listen(port, () => {

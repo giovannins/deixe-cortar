4aea0af0828562a61fee5615ff80f4889e016386
Gabriel Neves <ga.sadauscas@gmail.com>
2024-11-12 13:34:46 -0300

    Feat Baixei o Prisma e o Dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
+
+dotenv.config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
+const prisma = new PrismaClient()
 
 app.use(cors())
 app.use(express.json())

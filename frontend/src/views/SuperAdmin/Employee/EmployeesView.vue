9ceb9e9bd505cb6017954fee286cf06765255acc
Giovanni Neves <giovanni_neves@protonmail.com>
2024-12-07 14:32:46 -0300

    Feat: Update EmployeesView to display employee details and add delete confirmation modal

--- frontend/src/views/SuperAdmin/Employee/EmployeesView.vue
+++ frontend/src/views/SuperAdmin/Employee/EmployeesView.vue
@@ -1,45 +1,54 @@
 <script setup>
-import { useBarbershopStore } from '@/stores/barbershop'
+import { useEmployeeStore } from '@/stores/employee'
+import { useRoute } from 'vue-router'
+const route = useRoute()
 
-const barbershopStore = useBarbershopStore()
-barbershopStore.getBarbershops()
+const employeeStore = useEmployeeStore()
+employeeStore.findEmployeeById(route.params.id)
+
+function confirmDelete() {
+  document.getElementById('modal').classList.add('is-active')
+}
+
+function closeModel() {
+  document.getElementById('modal').classList.remove('is-active')
+}
 </script>
 
 <template>
+<div id="modal" class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-content">
+    <div class="box">
+      <p class="block">Are you sure you want to delete this Employee ({{ employeeStore.employee.name }})?</p>
+      <div class="buttons my-2">
+        <button class="button is-danger">Yes</button>
+        <button class="button" @click="closeModel">No</button>
+      </div>
+    </div>
+  </div>
+</div>
+
   <section class="hero is-link">
     <div class="hero-body">
-      <p class="title">Barbershops</p>
-      <p class="subtitle">All barbershops registered</p>
+      <p class="title">Employee</p>
+      <p class="subtitle">{{ employeeStore.employee.name }}</p>
     </div>
   </section>
   <section class="container is-fluid mt-6">
-    <div class="table-container">
-      <table class="table is-striped is-fullwidth is-hoverable">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Active</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="barbershop in barbershopStore.barbershops" :key="barbershop.id">
-            <td>{{ barbershop.name }}</td>
-            <td>{{ barbershop.email }}</td>
-            <td>{{ barbershop.phone }}</td>
-            <td><input type="checkbox" name="active" id="active" disabled v-model="barbershop.active" /></td>
-            <td>
-              <router-link
-                :to="{ name: 'barbershop-detail', params: { id: barbershop.id } }"
-                class="button is-warning mx-1"
-                >View</router-link
-              >
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="card">
+  <div class="card-content">
+    <div class="content">
+      <p>Name: {{ employeeStore.employee.name }}</p>
+      <p>Phone: {{ employeeStore.employee.phone }}</p>
+      <p>Note: {{ employeeStore.employee.note }}</p>
     </div>
+  </div>
+  <footer class="card-footer buttons has-addons">
+    <router-link :to="{name:'barbershop-detail', params: {id: employeeStore.employee.barbershopId}}"  class="card-footer-item button is-primary">View barbershop</router-link>
+    <router-link class="card-footer-item button is-warning">Edit</router-link>
+    <button @click="confirmDelete" class="card-footer-item button is-danger">Delete</button>
+  </footer>
+</div>
   </section>
 </template>

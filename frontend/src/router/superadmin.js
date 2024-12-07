73c45c0566113c7b4a9fcaeb2c4d6be7c304ff1e
Giovanni Neves <giovanni_neves@protonmail.com>
2024-12-07 14:32:38 -0300

    Feat: Add employee and service detail views to superadmin router

--- frontend/src/router/superadmin.js
+++ frontend/src/router/superadmin.js
@@ -1,6 +1,8 @@
 import BarbershopsDash from '@/views/SuperAdmin/Barbershop/BarbershopsDash.vue'
 import BarbershopsView from '@/views/SuperAdmin/Barbershop/BarbershopsView.vue'
 import DashBoard from '@/views/SuperAdmin/DashBoard.vue'
+import EmployeesView from '@/views/SuperAdmin/Employee/EmployeesView.vue'
+import ServicesView from '@/views/SuperAdmin/Service/ServicesView.vue'
 
 export default [
   {
@@ -17,5 +19,15 @@ export default [
     path: '/superadmin/barbershop/:id',
     name: 'barbershop-detail',
     component: BarbershopsView,
+  },
+  {
+    path: '/superadmin/employee/:id',
+    name: 'employee-detail',
+    component: EmployeesView,
+  },
+  {
+    path: '/superadmin/service/:id',
+    name: 'service-detail',
+    component: ServicesView,
   }
 ]
